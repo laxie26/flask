2296753066e0cad7f45f10e6770b8502998be346
laxie26 <143947051+laxie26@users.noreply.github.com>
2023-09-09 23:15:08 +0530

    Add files via upload

---
+++ app1.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, render_template
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form['name']
+    email = request.form['email']
+    course = request.form['course']
+
+    # Connect to the SQLite database
+    mydb = sqlite3.connect('mydatabase4.db')
+    cur = mydb.cursor()
+
+    # Insert data into the 'customers' table
+    query = "INSERT INTO customers (name, email, course) VALUES (?, ?, ?)"
+    cur.execute(query, (name, email, course))
+    mydb.commit()
+    mydb.close()
+
+    return "Data submitted successfully!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
